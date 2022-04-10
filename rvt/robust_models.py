07149130500f69879e681cbc28d818f858cbdfae
Gary-Shi <1159516387@pku.edu.cn>
2022-04-09 19:34:11 -0700

    bug

--- rvt/robust_models.py
+++ rvt/robust_models.py
@@ -92,7 +92,7 @@ class Attention(nn.Module):
 
 class Block(nn.Module):
 
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+    def __init__(self, dim, num_heads, image_size=14, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, use_mask=False, no_residual=False):
         super().__init__()
         self.norm1 = norm_layer(dim)
